591b286cb3c73a9135acae112921ed191fe67de0
Isaac Flores-Huerta <isaac.floreshuerta@demexchange.com>
2021-05-12 00:38:36 -0400

    style landing page

--- pages/index.js
+++ pages/index.js
@@ -2,49 +2,47 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-dark-blue">
       <Head>
         <title>Masa Madre</title>
         <link rel="icon" href="/sourdough.png" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <div className="grid grid-cols-3 divide-x-8 divide-ruby">
+          <div className="col-span-2"></div>
+          <div className="text-off-white relative h-20 w-96">
+            <div className="absolute top-0 right-0 h-20 w-96">
+              <button className="btn bg-dim-gray font-bold text-off-white py-3 px-6 inline-flex items-center rounded">
+                {/* <svg class="fill-current text-off-white w-4 h-4 mr-2" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
+                  <path d="M58.0221182,5048.95372 C55.8242503,5048.95372 54.0368636,5047.16802 54.0368636,5044.97223 C54.0368636,5042.77644 55.8242503,5040.99074 58.0221182,5040.99074 C60.2199861,5040.99074 62.0073727,5042.77644 62.0073727,5044.97223 C62.0073727,5047.16802 60.2199861,5048.95372 58.0221182,5048.95372 M58.0221182,5039 C54.7203348,5039 52.0442363,5041.67357 52.0442363,5044.97223 C52.0442363,5046.26621 52.4606954,5047.46165 53.1611039,5048.4401 L44,5057.59254 L45.4087875,5059 L46.8295307,5057.5806 L48.2104214,5058.96019 L49.6202052,5057.55273 L48.2383182,5056.17314 L50.2100229,5054.2033 L51.6108399,5055.6028 L53.0196274,5054.19534 L51.6188104,5052.79585 L54.5728803,5049.84457 C55.5482714,5050.53536 56.7358773,5050.94446 58.0221182,5050.94446 C61.3239016,5050.94446 64,5048.27089 64,5044.97223 C64,5041.67357 61.3239016,5039 58.0221182,5039"/>
+                </svg> */}
+                <span>Login</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 justify-right">
+          <p className="text-off-white font-bold text-9xl relative w-96">Masa</p>
+          <div></div>
+        </div>
+        <div className="grid grid-cols-2">
+          <div>
+            <img className="object-scale-down h-72 w-full" src="/folding.jpg" alt="Folding Bread"></img>
+            <p className="text-off-white font-bold text-9xl">Madre</p>
+          </div>
+          <div className="border-solid border-12 border-r-0">
+            <img src="/overview.jpg" alt="Top Down Shot of Sourdough Starter"></img>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 divide-x-64 divide-ruby">
+          <div className="relative h-20 w-96">
+            <p className="text-off-white text-left text-xl left-0">We'll help you get the most out </p>
+            <p className="text-off-white text-left text-xl left-0">of your starter by suggesting</p>
+            <p className="text-off-white text-left text-xl left-0">when to feed it and</p>
+            <p className="text-off-white text-left text-xl left-0">how best to take care of it.</p>
+          </div>
+          <div className="col-span-3"></div>
         </div>
       </main>
 
@@ -53,159 +51,10 @@ export default function Home() {
           href="https://www.flores-huerta.com"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          className="text-off-white">
           Created By: Isaac
         </a>
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          background-color: #12162B;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          background-color: #12162B;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: #EFEFED;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #D51E58;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-          color: #EFEFED;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #5D6360;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #5D6360;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #D51E58;
-          border-color: #EFEFED;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,47 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: theme => ({
+      'dark-blue': '#12162B',
+      'off-white': '#EAEAEA',
+      'ruby': '#D51E58',
+      'dim-gray': '#5D6360'
+    }),
+    textColor: theme => ({
+      'dark-blue': '#12162B',
+      'off-white': '#EAEAEA',
+      'ruby': '#D51E58',
+      'dim-gray': '#5D6360'
+    }),
+    divideColor: theme => ({
+      'dark-blue': '#12162B',
+      'off-white': '#EAEAEA',
+      'ruby': '#D51E58',
+      'dim-gray': '#5D6360'
+    }),
+    borderWidth: theme => ({
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '12': '12px'
+    }),
+    divideWidth: theme => ({
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '12': '12px',
+      '64': '64px'
+    }),
+    
     extend: {},
   },
   variants: {
