178cbf40d4b6bcd1cb30c57e9514653652cd4fa6
Isaac Flores-Huerta <isaac.floreshuerta@demexchange.com>
2021-05-13 21:48:10 -0400

    overview stats to footer

--- pages/overview.js
+++ pages/overview.js
@@ -5,6 +5,10 @@ const userName = 'Isaac'
 const timeSinceLastFeed = '5 Hours'
 const strength = 'Medium'
 const hydration = '100'
+const temp = '72F'
+const humidity = '54%'
+const co2 = '1200 ppm'
+const height = '52 mm'
 
 const NavItem = props => (
     <li className="block">
@@ -30,6 +34,8 @@ export default function Analytics() {
                             <NavItem href="/analytics" text="Analytics" />
                             <NavItem href="/feed" text="Feed" />
                             <NavItem href="/recipes" text="Recipes" />
+                            <NavItem href="/recipes" text="Profile" />
+                            <NavItem href="/recipes" text="Settings" />
                         </ul>
                     </nav>
                 </div>
@@ -37,13 +43,38 @@ export default function Analytics() {
             </div>
             <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
                 <div>
-                    <p className="text-8xl text-dark-blue font-extrabold text-left">Welcome Back {userName}! 
-                    It has been <span className="italic underline bold font-medium hover:bg-ruby">{timeSinceLastFeed}</span> since you last fed your starter. 
-                    Currently it's at <span className="italic underline bold font-medium hover:bg-ruby">{strength}</span> strength. 
+                    <p className="text-8xl text-dark-blue font-extrabold text-left">Welcome Back {userName}!
+                    It has been <span className="italic underline bold font-medium hover:bg-ruby">{timeSinceLastFeed}</span> since you last fed your starter.
+                    Currently it's at <span className="italic underline bold font-medium hover:bg-ruby">{strength}</span> strength.
                     Your current starter is <span className="italic underline bold font-medium hover:bg-ruby">{hydration}%</span> hydration.</p>
                 </div>
-
             </main>
+            <footer className="flex flex-col px-5">
+                <div className="grid grid-cols-10">
+                    <div className="col-span-3 bg-ruby group inline-block w-56 sticky bottom-0">
+                        <div className="flex px-3 py-2 dropdown-menu group inline-flex items-center">
+                            <div className="space-y-2 hidden group-hover:block bg-ruby">
+                                <p>eCO2: <span>{co2}</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-5 bg-dim-gray group inline-block w-36 sticky bottom-0">
+                        <div className="flex px-3 py-2 dropdown-menu group inline-flex items-center">
+                            <div className="space-y-2 hidden group-hover:block dim-gray">
+                                <p>Height: <span>{height}</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-2 bg-dark-blue group inline-block w-80">
+                        <div className="flex px-3 py-2 dropdown-menu group inline-flex items-center">
+                            <div className="space-y-2 hidden group-hover:block bg-dark-blue">
+                                <p className="text-off-white">Temperature: <span>{temp}</span></p>
+                                <p className="text-off-white">Humidity: <span>{humidity}</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
