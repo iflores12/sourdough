e6db93aab314d106953db74a23151d38c5c5329c
Isaac Flores-Huerta <isaac.floreshuerta@demexchange.com>
2021-05-15 21:20:21 -0400

    more styling

--- pages/feed.js
+++ pages/feed.js
@@ -11,7 +11,7 @@ const co2 = '1200 ppm'
 const height = '52 mm'
 
 const NavItem = props => (
-    <li className="block">
+    <li className="block pb-3">
         <Link href={props.href}>
             <a className="text-lg font-bold text-off-white px-2 py-1 hover:bg-dim-gray hover:bg-opacity-75 rounded">
                 {props.text}
@@ -29,20 +29,20 @@ export default function Analytics() {
             <div className="grid grid-cols-5">
                 <div className="bg-ruby group inline-block fixed w-80">
                     <nav className="flex px-3 py-2 dropdown-menu group inline-flex items-center">
-                        <ul className="space-y-2 hidden group-hover:block bg-ruby">
+                        <ul className="space-y-2 hidden group-hover:block bg-ruby pt-4 pb-6">
                             <NavItem href="/overview" text="Overview" />
                             <NavItem href="/analytics" text="Analytics" />
                             <NavItem href="/feed" text="Feed" />
                             <NavItem href="/recipes" text="Recipes" />
-                            <NavItem href="/recipes" text="Profile" />
-                            <NavItem href="/recipes" text="Settings" />
+                            <NavItem href="/profile" text="Profile" />
+                            <NavItem href="/settings" text="Settings" />
                         </ul>
                     </nav>
                 </div>
                 <div className="col-span-4"></div>
             </div>
             <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-                <div className="flex border-solid border-8 border-ruby px-60 py-8 m-10">
+                <div className="flex border-solid border-8 border-ruby rounded px-60 py-8 m-10">
                     <form action="" className="">
                         <div className="flex border-b border-dark-blue">
                             <label for="hydration" className="text-right mr-1 px-2 py-2">Hydration</label>
@@ -52,23 +52,25 @@ export default function Analytics() {
                             <label for="Flours Added" className="text-right mr-1 px-2 py-2">Flours</label>
                             <input type="text" id="flour" name="flour" placeholder="50% AP, 40% Whole Wheat, 10% Rye" className="flex-1 bg-transparent"/>
                         </div>
-                        <button className="justify-left block w-32 py-3 text-off-white font-bold bg-ruby rounded shadow-md focus:ring-2 hover:bg-opacity-90">Feed</button>
+                        <div className="px-32 pt-4">
+                            <button className="justify-right block w-32 py-3 text-off-white font-bold bg-ruby rounded shadow-md focus:ring-2 hover:bg-opacity-90">Feed</button>
+                        </div>
                     </form>
                 </div>
                 <div className="space-y-6">
-                <div className="flex border-solid border-8 border-dark-blue px-60 py-8">
+                <div className="flex border-solid border-4 border-dark-blue px-60 py-8 rounded">
                     FEED1
                 </div>
-                <div className="flex border-solid border-8 border-dark-blue px-60 py-8">
+                <div className="flex border-solid border-4 border-dark-blue px-60 py-8 rounded">
                     FEED2
                 </div>
-                <div className="flex border-solid border-8 border-dark-blue px-60 py-8">
+                <div className="flex border-solid border-4 border-dark-blue px-60 py-8 rounded">
                     FEED3
                 </div>
-                <div className="flex border-solid border-8 border-dark-blue px-60 py-8">
+                <div className="flex border-solid border-4 border-dark-blue px-60 py-8 rounded">
                     FEED4
                 </div>
-                <div className="flex border-solid border-8 border-dark-blue px-60 py-8">
+                <div className="flex border-solid border-4 border-dark-blue px-60 py-8 rounded">
                     FEED5
                 </div>
                 </div>
